ad39ad9a713f160d9c9f34ab8d5cfef6f78bf71d
K9-guardian <girkar.km@gmail.com>
2022-09-24 17:25:06 -0700

    Automatically pull first result

--- .config/nvim/after/plugin/nvim-cmp.lua
+++ .config/nvim/after/plugin/nvim-cmp.lua
@@ -12,11 +12,9 @@ cmp.setup({
       end,
    },
    mapping = cmp.mapping.preset.insert({
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }),
    }),
    sources = cmp.config.sources({
       { name = "nvim_lsp" },
