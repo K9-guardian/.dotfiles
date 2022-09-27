9b307046cdad6b977a7fb0c2b306b49b4b14042c
K9-guardian <girkar.km@gmail.com>
2022-09-26 17:45:40 -0700

    Idk how to get sioyek to work

--- .config/nvim/after/plugin/vimtex.lua
+++ .config/nvim/after/plugin/vimtex.lua
@@ -1,5 +1,4 @@
-vim.g.vimtex_view_method = "sioyek"
-vim.g.vimtex_view_sioyek_exe = "Sioyek-x86_64.AppImage"
+vim.g.vimtex_view_method = "zathura"
 
 vim.g.vimtex_quickfix_open_on_warning = 0
 vim.g.vimtex_quickfix_autoclose_after_keystrokes = 1
