7b2d14f0acb1b066ebe7cc54149cc3341461c020
K9-guardian <girkar.km@gmail.com>
2022-09-28 02:05:09 -0700

    Use cool lua snips

--- .config/nvim/after/plugin/luasnip.lua
+++ .config/nvim/after/plugin/luasnip.lua
@@ -1,8 +1,9 @@
 local luasnip = require("luasnip")
+luasnip.config.setup({ enable_autosnippets = true })
 
 vim.keymap.set({ "i", "s" }, "jk", function() luasnip.jump(1) end)
 
-require("luasnip.loaders.from_snipmate").lazy_load()
+require("luasnip.loaders.from_lua").lazy_load({ paths = "./snippets" })
 
 vim.api.nvim_create_user_command("EditSnippets", function(_)
    require("luasnip.loaders").edit_snippet_files({})
