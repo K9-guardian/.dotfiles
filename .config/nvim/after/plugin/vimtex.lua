0da674bc1951c6d1d2974b16caeeb05026756e9e
K9-guardian <girkar.km@gmail.com>
2022-09-28 17:35:16 -0700

    Switch vimscript function to lua

--- .config/nvim/after/plugin/vimtex.lua
+++ .config/nvim/after/plugin/vimtex.lua
@@ -8,22 +8,20 @@ vim.g.vimtex_indent_bib_enabled = 0
 
 vim.g.vimtex_imaps_leader = "@"
 
--- TODO: Use xdotool to resize terminal and viewer
-vim.cmd([[
-" Close viewers when VimTeX buffers are closed
-function! CloseViewers()
- if executable('xdotool')
-       \ && exists('b:vimtex.viewer.xwin_id')
-       \ && b:vimtex.viewer.xwin_id > 0
-   call system('xdotool windowclose '. b:vimtex.viewer.xwin_id)
- endif
-endfunction
+local function close_viewers()
+   if (os.execute("xdotool --version") == 0 and
+       vim.b.vimtex.viewer.xwin_id and
+       vim.b.vimtex.viewer.xwin_id ~= "0") then
+      os.execute("xdotool windowclose " .. vim.b.vimtex.viewer.xwin_id)
+   end
+end
 
+vim.cmd([[
 " Compile on initialization, cleanup on quit
 augroup vimtex
  au!
  au User VimtexEventQuit     VimtexClean
- au User VimtexEventQuit     call CloseViewers()
+ au User VimtexEventQuit     lua close_viewers()
  au User VimtexEventInitPost VimtexCompile
 augroup END
 ]])
