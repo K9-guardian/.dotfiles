cf1251cd08414329a4b751eb14155da2f0d49f58
K9-guardian <girkar.km@gmail.com>
2024-02-08 17:55:55 -0800

    change quickfix keybind and add diagnostic for treesitter

--- .config/nvim/lua/k9guardian/keymaps.lua
+++ .config/nvim/lua/k9guardian/keymaps.lua
@@ -2,7 +2,8 @@ vim.keymap.set("n", "<C-p>", "<Cmd>Files<CR>")
 
 vim.keymap.set("n", "]q", "<Cmd>cnext<CR>")
 vim.keymap.set("n", "[q", "<Cmd>cprevious<CR>")
-vim.keymap.set("n", "<Leader>cq", "<Cmd>cclose<CR>", { silent = false })
+vim.keymap.set("n", "<Leader>co", "<Cmd>copen<CR>", { silent = false })
+vim.keymap.set("n", "<Leader>cc", "<Cmd>cclose<CR>", { silent = false })
 
 vim.keymap.set("v", "<Enter>", "<Plug>(EasyAlign)")
 vim.keymap.set("n", "ga", "<Plug>(EasyAlign)")
