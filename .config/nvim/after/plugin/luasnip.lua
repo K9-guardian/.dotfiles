2af0bcd18dca05d91575e731042ce0bb1d63e981
K9-guardian <girkar.km@gmail.com>
2024-11-12 23:14:50 -0800

    Make luasnip tab fallback to inserting tab

--- .config/nvim/after/plugin/luasnip.lua
+++ .config/nvim/after/plugin/luasnip.lua
@@ -2,8 +2,8 @@ local luasnip = require("luasnip")
 luasnip.config.setup({ enable_autosnippets = true })
 
 vim.keymap.set({ "i", "s" }, "<Tab>", function()
-   if luasnip.jumpable(1) then luasnip.jump(1) end
-end)
+   if luasnip.jumpable(1) then luasnip.jump(1) else return "<Tab>" end
+end, { expr = true })
 vim.keymap.set({ "i", "s" }, "<S-Tab>", function()
    if luasnip.jumpable(-1) then luasnip.jump(-1) end
 end)
