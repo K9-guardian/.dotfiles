0f8ccd78ca7c1d4332c6efeeca6179fe8c34e6da
Krishna Girkar <girkar.km@gmail.com>
2025-03-28 20:18:15 -0700

    ensure installed treesitter

--- dot-config/nvim/after/plugin/treesitter.lua
+++ dot-config/nvim/after/plugin/treesitter.lua
@@ -1,5 +1,16 @@
 ---@diagnostic disable-next-line: missing-fields
 require("nvim-treesitter.configs").setup {
+   ensure_installed = {
+      "clojure",
+      "haskell",
+      "java",
+      "javascript",
+      "latex",
+      "prolog",
+      "python",
+      "racket",
+      "rust",
+   },
    highlight = { enable = true },
    rainbow = {
       enable = true,
