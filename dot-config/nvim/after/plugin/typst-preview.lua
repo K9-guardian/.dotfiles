df52471a242f27fa84d174efde12439d637460be
Krishna Girkar <girkar.km@gmail.com>
2025-04-10 18:57:25 -0700

    fix for fedora

--- dot-config/nvim/after/plugin/typst-preview.lua
+++ dot-config/nvim/after/plugin/typst-preview.lua
@@ -1,7 +1,7 @@
 require("typst-preview").setup {
    -- Custom format string to open the output link provided with %s
    -- Example: open_cmd = 'firefox %s -P typst-preview --class typst-preview'
-   open_cmd = "chromium %s",
+   open_cmd = "chromium-browser %s",
 
    -- Setting this to 'always' will invert black and white in the preview
    -- Setting this to 'auto' will invert depending if the browser has enable
