9d3d1363c232a424643e13e562c625474d2df6ec
Krishna Girkar <girkar.km@gmail.com>
2025-08-02 23:31:40 -0700

    Fix server neovim

--- dot-config/nvim/lua/k9guardian/plugins.lua
+++ dot-config/nvim/lua/k9guardian/plugins.lua
@@ -3,7 +3,6 @@
 return require("packer").startup(function(use)
    use "wbthomason/packer.nvim"
    -- Theme {{{
-   use "HiPhish/rainbow-delimiters.nvim"
    use "folke/tokyonight.nvim"
    -- }}}
 
@@ -19,30 +18,5 @@ return require("packer").startup(function(use)
    use "tpope/vim-surround"
    use "tpope/vim-unimpaired"
    use { "ibhagwan/fzf-lua", requires = { "nvim-tree/nvim-web-devicons" } }
-   -- }}}
-
-   -- Programming {{{
-   use "L3MON4D3/LuaSnip"
-   use "Olical/conjure"
-   use "hrsh7th/cmp-buffer"
-   use "hrsh7th/cmp-cmdline"
-   use "hrsh7th/cmp-nvim-lsp"
-   use "hrsh7th/cmp-path"
-   use "williamboman/mason.nvim"
-   use "williamboman/mason-lspconfig.nvim"
-   use "hrsh7th/nvim-cmp"
-   use "neovim/nvim-lspconfig"
-   use "saadparwaiz1/cmp_luasnip"
-   use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
-   -- }}}
-
-   -- Languages {{{
-   use "adimit/prolog.vim"
-   use "clojure-vim/vim-jack-in"
-   use "fladson/vim-kitty"
-   use "lervag/vimtex"
-   use "wlangstroth/vim-racket"
-   use "kaarmu/typst.vim"
-   use { "chomosuke/typst-preview.nvim", tag = 'v1.*' }
    -- }}}
 end)
