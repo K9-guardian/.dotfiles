a227f33192bbb4dd4239f568b94d649a743057bf
Krishna Girkar <girkar.km@gmail.com>
2025-09-03 22:14:03 -0700

    Add osc52 clipboard

--- dot-config/nvim/lua/k9guardian/settings.lua
+++ dot-config/nvim/lua/k9guardian/settings.lua
@@ -1,6 +1,18 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+vim.g.clipboard = {
+   name = "OSC 52",
+   copy = {
+      ['+'] = require("vim.ui.clipboard.osc52").copy('+'),
+      ['*'] = require("vim.ui.clipboard.osc52").copy('*'),
+   },
+   paste = {
+      ['+'] = require("vim.ui.clipboard.osc52").paste('+'),
+      ['*'] = require("vim.ui.clipboard.osc52").paste('*'),
+   },
+}
+
 vim.opt.clipboard = { "unnamedplus" }
 
 vim.opt.completeopt = { "menu", "noinsert", "noselect" }
