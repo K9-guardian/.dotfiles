3bec457ac25f8d0ecf698bc593dc827287e7649c
Krishna Girkar <girkar.km@gmail.com>
2025-12-22 12:31:04 -0800

    add keymaps from main

--- dot-config/nvim/lua/k9guardian/keymaps.lua
+++ dot-config/nvim/lua/k9guardian/keymaps.lua
@@ -12,4 +12,52 @@ vim.keymap.set("n", "<C-w>f", "<Cmd>vertical wincmd f<CR>")
 vim.keymap.set("v", "<C-w><C-F>", "<Cmd>vertical wincmd f<CR>")
 vim.keymap.set("v", "<C-w>f", "<Cmd>vertical wincmd f<CR>")
 
+-- Add jumps for paging
+vim.keymap.set("n", "<C-d>", "m'<C-d>")
+vim.keymap.set("n", "<C-u>", "m'<C-u>")
+vim.keymap.set("n", "<C-f>", "m'<C-f>")
+vim.keymap.set("n", "<C-b>", "m'<C-b>")
+
+-- Returns a function that checks if motion was used with a count.
+-- If so, add position to the jumplist. Useful for big relative jumps like "20j".
+local function count_motion_with_jump(motion)
+   return function()
+      local count = vim.v.count1
+      if count > 1 then
+         vim.cmd("normal! m'")
+      end
+      vim.cmd("normal! " .. count .. motion)
+   end
+end
+
+vim.keymap.set("n", "j", count_motion_with_jump("j"))
+vim.keymap.set("n", "k", count_motion_with_jump("k"))
+
+-- Returns a function that search for the word under the cursor.
+-- If the cursor is already on the word, keep the cursor there.
+-- Otherwise, move the cursor to the next occurrence.
+local function search_word_anchor(flags)
+   return function()
+      flags = flags or ""
+      local current_hlsearch = vim.v.hlsearch
+      local word = vim.fn.expand("<cword>")
+      local pattern = "\\<" .. vim.fn.escape(word, "\\/.*$^~[]") .. "\\>"
+      local last_pattern = vim.fn.getreg("/")
+
+      vim.fn.setreg("/", pattern)
+      vim.v.hlsearch = true
+
+      vim.cmd("normal! m'")
+
+      if last_pattern == pattern and current_hlsearch ~= 0 then
+         vim.fn.search(pattern, flags .. "W")
+      else
+         vim.fn.search(pattern, flags .. "cn")
+      end
+   end
+end
+
+vim.keymap.set("n", "*", search_word_anchor())
+vim.keymap.set("n", "#", search_word_anchor("b"))
+
 vim.api.nvim_create_user_command("W", "write", {})
