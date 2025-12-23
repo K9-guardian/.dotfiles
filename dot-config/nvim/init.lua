3ea3a24d0304b342d19106decc86dc9d831c32f5
Krishna Girkar <girkar.km@gmail.com>
2025-12-22 23:41:04 -0800

    simpler reload, still breaks tho :shrug:

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -1,10 +1,4 @@
--- Source - https://stackoverflow.com/a/76199012
--- Posted by Yanis.F
--- Retrieved 2025-12-15, License - CC BY-SA 4.0
-
---- Reload the entire configuration
 function ReloadConfig()
-   -- Reload k9guardian/ directory
    for name, _ in pairs(package.loaded) do
       if name:match('^k9guardian') then
          package.loaded[name] = nil
@@ -12,15 +6,6 @@ function ReloadConfig()
    end
 
    dofile(vim.env.MYVIMRC)
-
-   -- Reload after/ directory
-   local glob = vim.fn.stdpath('config') .. '/after/**/*.lua'
-   local after_lua_filepaths = vim.fn.glob(glob, true, true)
-   for _, filepath in ipairs(after_lua_filepaths) do
-      dofile(filepath)
-   end
-
-   vim.notify("Nvim configuration reloaded!", vim.log.levels.INFO)
 end
 
 require("k9guardian.settings")
