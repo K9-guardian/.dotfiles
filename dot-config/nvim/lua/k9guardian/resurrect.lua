e352e9eed70ca31cd05a4209f61eae08a7c6952b
Krishna Girkar <girkar.km@gmail.com>
2026-04-21 14:33:10 -0700

    better names

--- dot-config/nvim/lua/k9guardian/resurrect.lua
+++ dot-config/nvim/lua/k9guardian/resurrect.lua
@@ -1,25 +1,25 @@
-local win_snapshots = {}
+local window_snapshots = {}
 local closed_stack = {}
 
-local function is_real_buffer(buf)
-   return vim.bo[buf].buftype == "" and vim.api.nvim_buf_get_name(buf) ~= ""
+local function is_named_file_buffer(buffer)
+   return vim.bo[buffer].buftype == "" and vim.api.nvim_buf_get_name(buffer) ~= ""
 end
 
-local function snapshot_current_win()
-   local buf = vim.api.nvim_get_current_buf()
-   local win = vim.api.nvim_get_current_win()
-   if is_real_buffer(buf) then
-      win_snapshots[win] = vim.api.nvim_buf_get_name(buf)
+local function update_window_snapshots()
+   local buffer = vim.api.nvim_get_current_buf()
+   local window = vim.api.nvim_get_current_win()
+   if is_named_file_buffer(buffer) then
+      window_snapshots[window] = vim.api.nvim_buf_get_name(buffer)
    else
-      win_snapshots[win] = nil
+      window_snapshots[window] = nil
    end
 end
 
-local function on_win_closed(event)
-   local win_id = tonumber(event.match)
-   if not win_snapshots[win_id] then return end
-   table.insert(closed_stack, win_snapshots[win_id])
-   win_snapshots[win_id] = nil
+local function on_window_closed(event)
+   local window_id = tonumber(event.match)
+   if not window_snapshots[window_id] then return end
+   table.insert(closed_stack, window_snapshots[window_id])
+   window_snapshots[window_id] = nil
 end
 
 local function resurrect()
@@ -30,16 +30,16 @@ local function resurrect()
    vim.cmd("tabnew " .. vim.fn.fnameescape(table.remove(closed_stack)))
 end
 
-local group = vim.api.nvim_create_augroup("resurrect", { clear = true })
+local augroup = vim.api.nvim_create_augroup("resurrect", { clear = true })
 
 vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
-   group = group,
-   callback = snapshot_current_win,
+   group = augroup,
+   callback = update_window_snapshots,
 })
 
 vim.api.nvim_create_autocmd("WinClosed", {
-   group = group,
-   callback = on_win_closed,
+   group = augroup,
+   callback = on_window_closed,
 })
 
 vim.api.nvim_create_user_command(
